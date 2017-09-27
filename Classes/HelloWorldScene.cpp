901caba260892dfe819d5e54f527f1a062b87dee
AragonChen <jim39941857@gmail.com>
2017-09-27 16:30:47 +0800

    zzz

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -1,6 +1,11 @@
 #include "HelloWorldScene.h"
+#include "cocostudio/CocoStudio.h"
 
 USING_NS_CC;
+
+using namespace cocostudio::timeline;
+using namespace ui;
+using namespace CocosDenshion;
 
 Scene* HelloWorld::createScene()
 {
@@ -17,9 +22,20 @@ Scene* HelloWorld::createScene()
     return scene;
 }
 
+HelloWorld::~HelloWorld()
+{
+	// 課堂練習一
+#ifdef EXERCISE_1
+	_jumpAction->release();
+#endif
+}
+
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
     //////////////////////////////
     // 1. super init first
     if ( !Layer::init() )
@@ -27,60 +43,182 @@ bool HelloWorld::init()
         return false;
     }
     
-    Size visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    auto rootNode = CSLoader::createNode("MainScene.csb");
+    addChild(rootNode);
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-    
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
+// 音效與音樂 --------------------------------------------------------------------------------
+//	auto bkmusic = (cocostudio::ComAudio *)rootNode->getChildByName("bkmusic")->getComponent("bkmusic");
+//	bkmusic->playBackgroundMusic();
 
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
+	//SimpleAudioEngine::getInstance()->playBackgroundMusic("./music/SR_bg.mp3", true);
+	//SimpleAudioEngine::getInstance()->setBackgroundMusicVolume(0.2f);  //尚未實作
+	////SimpleAudioEngine::getInstance()->stopBackgroundMusic();	// 停止背景音樂
+	//SimpleAudioEngine::getInstance()->preloadEffect("thinking cloud.mp3");	// 預先載入音效檔
+	//unsigned int eid = SimpleAudioEngine::getInstance()->playEffect("thinking cloud.mp3",true);  // 播放音效檔
+	////SimpleAudioEngine::getInstance()->stopEffect(eid);  // 停止音效撥放，必須使用 PlayEffect 傳回的 id
+	////SimpleAudioEngine::getInstance()->unloadEffect("thinking cloud.mp3");  // 釋放音效檔
+//---------------------------------------------------------------------------------------------
 
-    /////////////////////////////
-    // 3. add your codes below...
+// Sprite ------------------------------------------------------------------------------------- 
+#ifndef EXERCISE_1
+	//auto runner = (cocos2d::Sprite *)rootNode->getChildByName("cuber01");
+	//runner->setColor(Color3B(75,155,77));
+	//Point pt = runner->getPosition();
+	//auto jumpAction = cocos2d::JumpTo::create(1.25f, Point(pt.x - 600, pt.y), 100, 3);
+	//auto tintTo = cocos2d::TintTo::create(1.25f, Color3B(50, 125, 250));
+	//runner->runAction(tintTo);
+	//runner->runAction(jumpAction);
 
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
+	// 課堂練習一
+#else
+	_runner = (cocos2d::Sprite *)rootNode->getChildByName("cuber01");
+	_runner->setColor(Color3B(75, 155, 77));
+	Point pt = _runner->getPosition();
+	_jumpAction = cocos2d::JumpTo::create(1.25f, Point(pt.x - 600, pt.y), 100, 3);
+	_jumpAction->retain();
+#endif
+//---------------------------------------------------------------------------------------------		
 
-    // add the label as a child to this layer
-    this->addChild(label, 1);
+// Button -------------------------------------------------------------------------------------
+	Button *cuberbtn = dynamic_cast<Button*>(rootNode->getChildByName("CuberBtn"));
+	//Button *cuberbtn = (cocos2d::ui::Button *)(rootNode->getChildByName("CuberBtn"));
+	cuberbtn->addTouchEventListener(CC_CALLBACK_2(HelloWorld::CuberBtnTouchEvent, this));
+//---------------------------------------------------------------------------------------------	
 
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
+// 文本 TTF 字型-------------------------------------------------------------------------------
+	//auto text = (cocos2d::ui::Text *)rootNode->getChildByName("Text101");
+	//text->setColor(Color3B(200, 255, 50));
+	// 以下用於 slider 顯示用
+	//_sliderValue = (cocos2d::ui::Text *)rootNode->getChildByName("Text101");
+//---------------------------------------------------------------------------------------------	
 
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+// FNT 字體-------------------------------------------------------------------------------
+	//auto bmtlabel = (cocos2d::Label *)rootNode->getChildByName("BMFont");
+	//bmtlabel->setColor(Color3B(200, 255, 50));
+//---------------------------------------------------------------------------------------------	
 
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
-    
-    return true;
+// CheckBox-------------------------------------------------------------------------------
+//	CheckBox *checkBox = dynamic_cast<CheckBox*>(rootNode->getChildByName("CheckBox_1"));
+////	CheckBox *checkBox = (cocos2d::ui::CheckBox*)(rootNode->getChildByName("CheckBox_1"));
+//	checkBox->addEventListener(CC_CALLBACK_2(HelloWorld::checkBoxTouchEvent, this));
+//---------------------------------------------------------------------------------------------	
+
+// loadingBar -------------------------------------------------------------------------------
+#if EXERCISE != 2
+	//auto loadingBar = (cocos2d::ui::LoadingBar *)rootNode->getChildByName("LoadingBar_1");
+	//loadingBar->setDirection(LoadingBar::Direction::LEFT);
+	//loadingBar->setPercent(50);
+#else //課堂練習二
+	_loadingBar = (cocos2d::ui::LoadingBar *)rootNode->getChildByName("LoadingBar_1");
+	_loadingBar->setDirection(LoadingBar::Direction::LEFT);
+	_loadingBar->setPercent(100);
+#endif
+//---------------------------------------------------------------------------------------------	
+
+	// 將 doStep 函式掛入 schedule list 中，每一個 frame 就都會被呼叫到
+	this->schedule(CC_SCHEDULE_SELECTOR(HelloWorld::doStep));
+
+
+// Slider 滑動條 -------------------------------------------------------------------------------------
+	//auto *slider = (cocos2d::ui::Slider *)(rootNode->getChildByName("Slider_1"));
+	//slider->addEventListener(CC_CALLBACK_2(HelloWorld::sliderEvent, this));
+	//slider->setPercent(50);
+	//slider->setMaxPercent(100);		// 可以設定超過 100
+//---------------------------------------------------------------------------------------------	
+
+//// 序列幀動畫 -------------------------------------------------------------------------------------
+//	auto runner = (cocos2d::Sprite *)rootNode->getChildByName("Sprite_1");
+//	runner->setColor(Color3B(135, 250, 50));
+//	auto action = (ActionTimeline *)CSLoader::createTimeline("MainScene.csb");
+//	rootNode->runAction(action);
+//	action->gotoFrameAndPlay(0, 21, true);
+//---------------------------------------------------------------------------------------------	
+
+// 讀入與控制組合式的序列幀動畫
+	//auto triRoot1 = CSLoader::createNode("triangleNode.csb");
+	//triRoot1->setPosition(100, 360);
+	//this->addChild(triRoot1);
+
+	//auto triAction = (ActionTimeline *)CSLoader::createTimeline("triangleNode.csb");
+	//triRoot1->runAction(triAction);
+	//triAction->setDuration(3.0f);
+	//triAction->gotoFrameAndPlay(0, 35, false);
+
+	//// _triRoot2 的動畫撥放在按下螢幕後啟動
+	//_triRoot2 = CSLoader::createNode("triangleNode.csb");
+	//_triRoot2->setPosition(300, 360);
+	//this->addChild(_triRoot2);
+	//_triAction = (ActionTimeline *)CSLoader::createTimeline("triangleNode.csb");
+	//_triAction->setDuration(2.0f);
+	//_triRoot2->runAction(_triAction);
+//---------------------------------------------------------------------------------------------	
+
+// 以下加入場景的 TouchBegan 事件中
+	//_triAction->gotoFrameAndPlay(0, 35, false);
+//---------------------------------------------------------------------------------------------	
+	return true;
 }
 
 
-void HelloWorld::menuCloseCallback(Ref* pSender)
+void HelloWorld::doStep(float dt)
 {
-    Director::getInstance()->end();
+#if EXERCISE == 2 //課堂練習二
+	float t = _loadingBar->getPercent();
+	if (t >= dt * 5 )  t = t - dt *5;
+	else t = 0;
+	_loadingBar->setPercent(t);
+#else
 
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
 #endif
+}
+
+void HelloWorld::CuberBtnTouchEvent(Ref *pSender, Widget::TouchEventType type)
+{
+	switch (type)
+	{
+		case Widget::TouchEventType::BEGAN:
+			log("Touch Down");
+			break;
+		case Widget::TouchEventType::MOVED:
+			log("Touch Move");
+			break;
+		case Widget::TouchEventType::ENDED:
+			log("Touch Up");
+#ifdef EXERCISE_1
+			_runner->runAction(_jumpAction);
+#endif
+			break;
+		case Widget::TouchEventType::CANCELED:
+			log("Touch Cancelled");
+			break;
+		default:
+			break;
+	}
+}
+
+void HelloWorld::checkBoxTouchEvent(Ref* object, CheckBox::EventType type) {
+	switch (type)
+	{
+	case CheckBox::EventType::UNSELECTED:
+		log("Unselected");
+		break;
+
+	case CheckBox::EventType::SELECTED:
+		log("Selected");
+		break;
+
+	}
+}
+
+
+void HelloWorld::sliderEvent(cocos2d::Ref* sender, cocos2d::ui::Slider::EventType type)
+{
+	if (type == Slider::EventType::ON_PERCENTAGE_CHANGED)
+	{
+		Slider* slider = dynamic_cast<Slider*>(sender);
+		int percent = slider->getPercent();
+		int maxPercent = slider->getMaxPercent();
+		_sliderValue->setString(StringUtils::format("Percent %5.2f", 100.0 * percent / maxPercent));
+	}
 }
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -2,18 +2,44 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include "ui/CocosGUI.h"
+#include "ui/UIWidget.h"
+#include "SimpleAudioEngine.h"
+#include "cocostudio/CocoStudio.h"
+
+#define EXERCISE 0
 
 class HelloWorld : public cocos2d::Layer
 {
-public:
-    static cocos2d::Scene* createScene();
+private:
+	// 課堂練習一
+#if EXERCISE == 1
+	cocos2d::Sprite *_runner;
+	cocos2d::JumpTo *_jumpAction;
+#elif EXERCISE == 2
+// loadingBar 
+	cocos2d::ui::LoadingBar *_loadingBar;
+#endif
 
+	cocos2d::ui::Text *_sliderValue;
+
+// 讀入與控制組合式的序列幀動畫
+	cocos2d::Node *_triRoot2;
+	cocostudio::timeline::ActionTimeline *_triAction;
+
+public:
+	~HelloWorld();
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScene();
+	void doStep(float dt);
+
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
-    
-    // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
-    
-    // implement the "static create()" method manually
+	void checkBoxTouchEvent(cocos2d::Ref* object, cocos2d::ui::CheckBox::EventType type);
+	void CuberBtnTouchEvent(cocos2d::Ref *pSender, cocos2d::ui::Widget::TouchEventType type);
+	void sliderEvent(cocos2d::Ref* sender, cocos2d::ui::Slider::EventType type);
+
+	// implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
 };
 
