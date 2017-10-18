86784404ca5381924304257a403a74d28db3f7ee
jim <ggg>
2017-10-18 16:32:19 +0800

    ...

---
+++ cocos2d/cocos/platform/android/java/gen/R.java.d
@@ -0,0 +1,1 @@
+ : D:\github\myfirst2dx\cocos2d\cocos\platform\android\java\bin\AndroidManifest.xml \
---
+++ proj.android/gen/R.java.d
@@ -0,0 +1,6 @@
+D:\github\myfirst2dx\proj.android\gen\org\cocos2dx\myfirst2dx\R.java \
+ : D:\github\myfirst2dx\proj.android\res\drawable-hdpi\icon.png \
+D:\github\myfirst2dx\proj.android\res\drawable-ldpi\icon.png \
+D:\github\myfirst2dx\proj.android\res\drawable-mdpi\icon.png \
+D:\github\myfirst2dx\proj.android\res\values\strings.xml \
+D:\github\myfirst2dx\proj.android\bin\AndroidManifest.xml \
